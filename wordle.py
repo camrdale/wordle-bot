bba641219230e7f6b866c59d2c1c6f38c95d67e9
Cameron Dale <camrdale@gmail.com>
2024-11-18 10:45:33 -0800

    Remove DB, back to in-memory dicts.

--- wordle.py
+++ wordle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 import os
 import functools
-import itertools
-import sqlite3
+import pickle
 import signal
 from tqdm import tqdm
+from scipy.stats import entropy # type: ignore
 from collections import defaultdict, Counter
+from typing import Any, Optional
 
 N_GUESSES = 10
 DICT_FILE = 'all_words.txt'
 SOLUTIONS_FILE = 'words.txt'
-DB_FILE = 'pattern_dict.db'
+DB_FILE = 'pattern_dict.p'
 SAVE_TIME = True
 
-def calculate_pattern(guess, true):
+def calculate_pattern(guess: str, true: str) -> tuple[int, ...]:
     """Generate a pattern list that Wordle would return if you guessed
     `guess` and the true word is `true`
     Thanks to MarkCBell, Jeroen van der Hooft and gbotev1
@@ -24,11 +25,11 @@ def calculate_pattern(guess, true):
     >>> calculate_pattern('rower', 'goner')
     (0, 2, 0, 2, 2)
     """
-    wrong = [i for i, v in enumerate(guess) if v != true[i]]
+    wrong: list[int] = [i for i, v in enumerate(guess) if v != true[i]]
     counts = Counter(true[i] for i in wrong)
-    pattern = [2] * 5
+    pattern: list[int] = [2] * 5
     for i in wrong:
-        v = guess[i]
+        v: str = guess[i]
         if counts[v] > 0:
             pattern[i] = 1
             counts[v] -= 1
@@ -37,7 +38,9 @@ def calculate_pattern(guess, true):
     return tuple(pattern)
 
 
-def generate_pattern_dict(dictionary, possible_solutions, db_file):
+def generate_pattern_dict(dictionary: list[str],
+                          possible_solutions: list[str]
+                          ) -> dict[str, dict[tuple[int, ...], set[str]]]:
     """For each word and possible information returned, store a list
     of candidate words
     >>> pattern_dict = generate_pattern_dict(['weary', 'bears', 'crane'])
@@ -46,68 +49,28 @@ def generate_pattern_dict(dictionary, possible_solutions, db_file):
     >>> sorted(pattern_dict['crane'][(0, 1, 2, 0, 1)])
     ['bears', 'weary']
     """
-    con = sqlite3.connect(db_file)
-    cur = con.cursor()
-    cur.execute('CREATE TABLE patterns (word TEXT NOT NULL, pattern TEXT NOT NULL, matches TEXT NOT NULL);')
+    pattern_dict: dict[str, dict[tuple[int, ...], set[str]]] = defaultdict(lambda: defaultdict(set))
     for word in tqdm(dictionary):
-        for solution in possible_solutions:
-            pattern = calculate_pattern(word, solution)
-            cur.execute('INSERT INTO patterns VALUES(?, ?, ?)',
-                        (word, ''.join(map(str, pattern)), solution))
-        con.commit()
-    cur.execute('CREATE INDEX pattern_index ON patterns(word, pattern);')
-    cur.execute('CREATE INDEX matches_index ON patterns(matches);')
-    con.close()
+        for word2 in possible_solutions:
+            pattern = calculate_pattern(word, word2)
+            pattern_dict[word][pattern].add(word2)
+    return dict(pattern_dict)
 
 
-def get_pattern_dict_macthes(pattern_dict, word, pattern):
-    cur = pattern_dict.cursor()
-    matches = set()
-    for row in cur.execute('SELECT matches FROM patterns WHERE word=? AND pattern=?',
-                           (word, ''.join(map(str, pattern)))):
-        matches.add(row[0])
-    return matches
-
-def calculate_entropies(words, remaining_solutions, pattern_dict):
+def calculate_entropies(words: list[str] | set[str],
+                        remaining_solutions: set[str], 
+                        pattern_dict: dict[str, dict[tuple[int, ...], set[str]]]
+                        ) -> list[tuple[str, float]]:
     """Calculate the entropy for every word in `words`, taking into account
     the `remaining_solutions`"""
     print('Calculating entropies for ', len(words), ' words')
+    entropies: list[tuple[str, float]] = []
+    for word in words:
+        entropies.append((word, entropy(
+            [len(matches.intersection(remaining_solutions)) 
+             for matches in pattern_dict[word].values()]))) # type: ignore
+    return entropies
 
-    cur = pattern_dict.cursor()
-    cur.execute('DROP TABLE IF EXISTS possible_words;')
-    cur.execute('CREATE TABLE possible_words (word TEXT NOT NULL PRIMARY KEY);')
-    cur.executemany('INSERT INTO possible_words VALUES(?)', zip(remaining_solutions))
-    pattern_dict.commit()
-
-    cur.execute('DROP TABLE IF EXISTS matches;')
-    cur.execute("""
-        CREATE TABLE matches AS
-          SELECT
-            p.word AS word,
-            COUNT(*) AS num_matches
-          FROM patterns AS p
-            INNER JOIN possible_words AS w
-              ON p.matches = w.word
-          GROUP BY p.word, pattern;""")
-    cur.execute('CREATE INDEX matches_words ON matches(word);')
-    pattern_dict.commit()
-
-    return [(row[0], row[1]) for row in cur.execute("""
-        SELECT
-          raw.word,
-          -SUM((num_matches/total_matches) * LOG(num_matches/total_matches)) AS entropy
-        FROM 
-          matches AS raw
-          INNER JOIN
-            (SELECT
-              word,
-              CAST(SUM(num_matches) AS REAL) AS total_matches
-            FROM matches
-            GROUP BY word
-            ) AS totals
-            ON raw.word = totals.word
-        GROUP BY raw.word
-        ORDER BY entropy DESC""")]
 
 abort = False
 
@@ -120,26 +83,32 @@ def main():
     with open(SOLUTIONS_FILE) as ifp:
         possible_solutions = list(map(lambda x: x.strip(), ifp.readlines()))
 
-    # Calculate the pattern_dict database if needed.
-    if DB_FILE not in os.listdir('.'):
-        generate_pattern_dict(all_dictionary, possible_solutions, DB_FILE)
-
-    pattern_dict = sqlite3.connect(DB_FILE)
+    # Calculate the pattern_dict and cache it, or load the cache.
+    if DB_FILE in os.listdir('.'):
+        print('Loading pattern dictionary from file')
+        pattern_dict: dict[str, dict[tuple[int, ...], set[str]]] = pickle.load(open(DB_FILE, 'rb'))
+    else:
+        print('Generating pattern dictionary')
+        pattern_dict = generate_pattern_dict(all_dictionary, possible_solutions)
+        print('Saving pattern dictionary to file')
+        pickle.dump(pattern_dict, open(DB_FILE, 'wb+'))
 
     # Cache of first round info's and the resulting guess.
-    initial_cache = {}
+    initial_cache: dict[tuple[int, ...], str] = {}
 
     # Overall accumulated stats across all words.
-    stats = defaultdict(list)
+    stats: dict[str, list[str | int]] = defaultdict(list)
 
     # Don't quit immediately on Ctrl-C, finish the iteratation and print the results.
-    def stop(signum, frame):
+    def stop(signum: int, frame: Optional[Any]):
         print('Aborting after this iteration')
         global abort
         abort = True
 
     signal.signal(signal.SIGINT, stop)
     signal.signal(signal.SIGTERM, stop)
+
+    info: tuple[int, ...] = ()
 
     for word_to_guess in tqdm(possible_solutions):
         if abort:
@@ -150,7 +119,7 @@ def main():
             remaining_solutions = set(possible_solutions)
             info = calculate_pattern(guess_word, word_to_guess)
             print('Guessing: ', guess_word, '  Info: ', info)
-            matches = get_pattern_dict_macthes(pattern_dict, guess_word, info)
+            matches = pattern_dict[guess_word][info]
             remaining_solutions = remaining_solutions.intersection(matches)
             init_round = 1
         else:
@@ -172,7 +141,7 @@ def main():
                       entropies = calculate_entropies(candidates, remaining_solutions, pattern_dict)
 
                   # Guess the candiate with highest entropy, preferring possible solutions to break ties
-                  def sorting(first, second):
+                  def sorting(first: tuple[str, float], second: tuple[str, float]):
                       if first[1] < second[1]:
                           return -1
                       if first[1] > second[1]:
@@ -181,7 +150,10 @@ def main():
                           return 1
                       if second[0] in remaining_solutions and first[0] not in remaining_solutions:
                           return -1
-                      return 0
+                      # Fall back to sorting by word so it is deterministic.
+                      if first[0] < second[0]:
+                          return -1
+                      return 1
                   entropies.sort(key=functools.cmp_to_key(sorting), reverse=True)
                   print('Top guesses: ', [(word, '{:.5f}'.format(entropy))
                                           for word, entropy in entropies[:6]])
@@ -189,8 +161,8 @@ def main():
                   if n_round == 1:
                       initial_cache[info] = guess_word
             else:
-                # One or two words remaining, choose the one, or pick randomly (can't do any better).
-                guess_word = list(remaining_solutions)[0]
+                # One or two words remaining, choose the one, or pick one (can't do any better).
+                guess_word = sorted(list(remaining_solutions))[0]
             info = calculate_pattern(guess_word, word_to_guess)
 
             # Print round information
@@ -203,13 +175,13 @@ def main():
                 break
 
             # Filter our list of remaining possible words
-            matches = get_pattern_dict_macthes(pattern_dict, guess_word, info)
+            matches = pattern_dict[guess_word][info]
             remaining_solutions = remaining_solutions.intersection(matches)
         else:
             stats['misses'].append(word_to_guess)
 
     print(len(stats['guesses']), 'successful guesses')
-    print('Average guess count:', 1.0 * sum(stats['guesses']) / len(stats['guesses']))
+    print('Average guess count:', 1.0 * sum(stats['guesses']) / len(stats['guesses'])) # type: ignore
     print('Guess counts of')
     print('  1:', len([x for x in stats['guesses'] if x == 1]))
     print('  2:', len([x for x in stats['guesses'] if x == 2]))
@@ -217,7 +189,7 @@ def main():
     print('  4:', len([x for x in stats['guesses'] if x == 4])) 
     print('  5:', len([x for x in stats['guesses'] if x == 5])) 
     print('  6:', len([x for x in stats['guesses'] if x == 6]))
-    print(' 7+:', len([x for x in stats['guesses'] if x > 6]))
+    print(' 7+:', len([x for x in stats['guesses'] if x > 6])) # type: ignore
     print('Missed words:', stats['misses'])
 
 if __name__ == "__main__":
